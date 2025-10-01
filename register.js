1314d6bedc7f41f23efcd1538072c771fdd3368b
Alma0205 <almacrespa10@gmail.com>
2025-10-01 16:10:26 -0500

    Update register.js

--- register.js
+++ register.js
@@ -5,61 +5,29 @@ const inputCorreo = document.getElementById("correo");
 const inputContraseña = document.getElementById("contraseña");
 const botonCrear = document.querySelector("button");
 
-let areaResultados = document.createElement("div");
-areaResultados.id = "resultados";
-areaResultados.style.marginTop = "20px";
-areaResultados.style.padding = "15px";
-areaResultados.style.background = "rgba(255,255,255,0.1)";
-areaResultados.style.borderRadius = "8px";
-areaResultados.style.textAlign = "center";
-areaResultados.innerText = "💀 Bienvenido, completa el formulario y presiona el botón 💀";
-document.querySelector(".registro-cuadro").appendChild(areaResultados);
+botonCrear.addEventListener("click", (event) => {
+  event.preventDefault();
 
-
-function mostrarDatos() {
-  // Obtener valores de los inputs
-  const nombre = inputNombre.value.trim();
-  const lugar = selectLugar.value;
-  const edad = inputEdad.value;
-  const correo = inputCorreo.value;
-  const contraseña = inputContraseña.value;
-
-
-  if (!nombre || lugar === "seleccionar" || !edad || !correo || !contraseña) {
-    areaResultados.textContent = "⚠️ Por favor, completa todos los campos.";
-    areaResultados.style.color = "orange";
+  if (!inputNombre.value || selectLugar.value === "seleccionar" ||
+      !inputEdad.value || !inputCorreo.value || !inputContraseña.value) {
+    alert("Por favor, completa todos los campos.");
     return;
   }
 
+  const nuevoUsuario = {
+    nombre: inputNombre.value,
+    lugar: selectLugar.value,
+    edad: inputEdad.value,
+    correo: inputCorreo.value,
+    contraseña: inputContraseña.value
+  };
 
-  areaResultados.innerHTML = "";
+  let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+  usuarios.push(nuevoUsuario);
+  localStorage.setItem("usuarios", JSON.stringify(usuarios));
 
-
-  let titulo = document.createElement("h3");
-  titulo.textContent = `🎬 Usuario Registrado 🎬`;
-  titulo.style.color = "rgb(255,56,56)";
-
-  let parrafo = document.createElement("p");
-  parrafo.innerHTML = `
-    <strong>Nombre:</strong> ${nombre} <br>
-    <strong>País:</strong> ${lugar} <br>
-    <strong>Edad:</strong> ${edad} años <br>
-    <strong>Correo:</strong> ${correo}
-  `;
+  localStorage.setItem("loggedUser", JSON.stringify(nuevoUsuario));
 
 
-  areaResultados.appendChild(titulo);
-  areaResultados.appendChild(parrafo);
-
- 
-  areaResultados.classList.add("activo");
-  setTimeout(() => {
-    areaResultados.classList.remove("activo");
-  }, 2000);
-}
-
-
-botonCrear.addEventListener("click", (event) => {
-  event.preventDefault(); 
-  mostrarDatos();
-});
+  window.location.href = "home.html";
+})
